6e7befa824ca1a85717083e24e02e1595b302ad5
SutirthaSaha <sutirthasaha12@gmail.com>
2018-01-19 21:20:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/splashscreendemo/SplashScreenActivity.java
@@ -0,0 +1,40 @@
+package com.example.dell.splashscreendemo;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        /*Thread myThread=new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(4000);
+                    Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();*/
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+        }
+    }
+
